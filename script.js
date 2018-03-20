b0236250fa017d27f2fea87fb46822cd6b1dce5c
Varrus <john.t.muraski@gmail.com>
2018-03-20 16:21:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//javascript script for Farkle
+//By: John Muraski
+// Copyright: 2018
+
+object diceRoll {
+    diceOne: 0;
+    diceTwo: 0;
+    diceThree: 0;
+    diceFour: 0;
+    diceFive: 0;
+    diceSix: 0;
+}
